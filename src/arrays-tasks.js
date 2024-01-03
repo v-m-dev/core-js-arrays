d1cde11794020bc5171b7a1b1a71543a872dcf83
v-m-dev <morozvitalii2403@gmail.com>
2024-01-03 18:38:42 +0200

    feat: complete getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -122,8 +122,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length < 1) return 0;
+  const total = arr.reduce((sum, num) => sum + num, 0);
+  return +(total / arr.length).toFixed(2);
 }
 
 /**
