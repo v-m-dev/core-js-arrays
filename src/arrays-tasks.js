59a33d936aaf1204f67b43a79c18f8ed470967e9
v-m-dev <morozvitalii2403@gmail.com>
2024-01-03 18:46:20 +0200

    feat: complete isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -138,8 +138,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const { length } = arr[0].split('');
+  return arr.every((v) => v.split('').length === length);
 }
 
 /**
